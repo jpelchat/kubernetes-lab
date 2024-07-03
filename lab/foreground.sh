50d44187862d10dbf3c4fdc8261ca5ce5f663671
jpelchat <83721525+jpelchat@users.noreply.github.com>
2024-07-03 09:13:14 -0400

    Update foreground.sh

--- lab/foreground.sh
+++ lab/foreground.sh
@@ -1,6 +1,14 @@
 echo "Setting up your lab...hang tight!"
 
-kubectl create namespace my-grafana
-curl https://pastebin.com/raw/Ed6XKTjE | kubectl apply --namespace=my-grafana -f - 
+# kubectl create namespace my-grafana
+# curl https://pastebin.com/raw/Ed6XKTjE | kubectl apply --namespace=my-grafana -f -
 
-nohup kubectl port-forward service/grafana 8080:8080 --address 0.0.0.0 --namespace=my-grafana > /dev/null 2> /dev/null &
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+helm install my-kube-prometheus-stack prometheus-community/kube-prometheus-stack
+
+kubectl expose service kube-prometheus-stack-prometheus --type=NodePort --target-port=9090 --name=prometheus-node-port-service
+kubectl expose service kube-prometheus-stack-grafana --type=NodePort --target-port=3000 --name=grafana-node-port-service
+
+kubectl get secret --namespace default kube-prometheus-stack-grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
