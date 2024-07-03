3d6b7668a85b735f25adb996487a80fbe942c9db
jpelchat <83721525+jpelchat@users.noreply.github.com>
2024-07-03 08:56:08 -0400

    Create foreground.sh

---
+++ lab/foreground.sh
@@ -0,0 +1,6 @@
+echo "Setting up your lab...hang tight!"
+
+kubectl create namespace my-grafana
+curl https://pastebin.com/raw/Ed6XKTjE | kubectl apply -f -
+
+nohup kubectl port-forward service/grafana 3000:3000 --address 0.0.0.0 --namespace=my-grafana &
